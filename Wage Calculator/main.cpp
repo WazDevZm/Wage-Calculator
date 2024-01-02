821f1d63833e9916e42d6147ab391228f3e79bc1
Wazingwa Mugala <121240982+WazDevZm@users.noreply.github.com>
2024-01-02 22:31:47 +0200

    Update main.cpp

--- Wage Calculator/main.cpp
+++ Wage Calculator/main.cpp
@@ -7,10 +7,10 @@ int main()
     double hour, rate, pay;
 
     cout <<"WELCOME TO WAZINGWA'S WAGE CALCULATOR   ";
-
+//The worker inputs the number of hours worked.
     cout <<"   How many hours did you work for?  ";
     cin >> hour;
-
+//The number worked per hour, input by the worker.
     cout << "What is your pay per hour? (PLEASE BE HONEST) ";
     cin >> rate;
 
